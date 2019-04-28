fcb5cbe86ddfa0ead9916149b70f691dfd754a81
rabinakh302 <rabinakh302@gmail.com>
2019-04-28 09:06:36 +0545

    Initial commit

---
+++ src/main/java/com/cvfiltering/project/cvfiltering/CvfilteringApplication.java
@@ -0,0 +1,13 @@
+package com.cvfiltering.project.cvfiltering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvfilteringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvfilteringApplication.class, args);
+	}
+
+}
